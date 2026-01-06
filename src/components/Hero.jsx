dac44ab099a10a9608f5a889faea9657078b4da4
Ankit Kumar <187834818+ankit8597286@users.noreply.github.com>
2026-01-06 22:23:01 +0530

    Add files via upload

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,39 +1,3 @@
-// import profile from "../assets/profile.png";
-// export default function Hero() {
-//   return (
-//     <section id="home" className="home">
-//       <div className="home-content">
-//         <h1>
-//           Hi, I Am <span>Ankit Kumar</span>
-//         </h1>
-//         <h2>
-//           And I'm a <span className="highlight">Programmer</span>
-//         </h2>
-
-//         <p>
-//           I am a dedicated BCA student with strong interest in programming,
-//           problem-solving and web development. I work with C, C++, Python,
-//           HTML, CSS and JavaScript.
-//         </p>
-
-//         <div className="home-buttons">
-//           <a href="#projects" className="btn-primary">
-//             View Projects
-//           </a>
-//           <a href="#contact" className="btn-secondary">
-//             Contact
-//           </a>
-//         </div>
-//       </div>
-
-//       <div className="home-image">
-//         <div className="profile-container">
-//           <img src={profile} alt="Ankit Kumar" />
-//         </div>
-//       </div>
-//     </section>
-//   );
-// }
 
 import profile from "../assets/profile.png";
 
